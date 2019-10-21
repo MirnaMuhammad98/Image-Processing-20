d002aa18d1360caa751f76778244e9f0e6ac2be0
emanalaa <eman3laa@outlook.com>
2019-10-22 01:15:02 +0200

    Implement Gaussian Filter

---
+++ Tasks/Task 2/Gauss2.m
@@ -0,0 +1,18 @@
+function M = Gauss2 (Sig)
+  N = floor(3.7 * Sig - 0.5);
+  Size = 2 * N + 1;
+  GaussMask = ones(Size, Size);
+  sigmaSquared = Sig^2;
+  x = -1 * (Size / 2);
+  y = -1 * (Size / 2);
+  for i=1:Size
+    for j=1:Size
+      equation = -1 * (x^2 + y ^2) / (2 * sigmaSquared);
+      GaussMask(i,j) = (1/(2*pi*sigmaSquared)) * exp(equation);
+      x += 1;
+      y += 1;
+    end;
+  end; 
+  M = (1/ sum(GaussMask(:))) * GaussMask;
+endfunction
+
