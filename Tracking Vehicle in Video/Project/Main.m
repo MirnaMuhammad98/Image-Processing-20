839b3c366dd5aa1021d0dd2aafc6556a476f2e2e
mennaawad <mennamawad16@gmail.com>
2019-12-21 03:09:12 +0200

    drawing line on images and update get centers of objects
    drawing line

--- Tracking Vehicle in Video/Project/Main.m
+++ Tracking Vehicle in Video/Project/Main.m
@@ -6,10 +6,12 @@
 % Difference between frames
 % Bounding Box
 
-case1 = ReadVideo('Case1/case1.wmv');
+%case1 = ReadVideo('Case1/case1.wmv');
+case1 = ReadVideo('Case2/case2.avi');
 % background1 = ExtractBackground(case1, 'background2.jpg');
-background1 = imread('background1.jpg');
+background1 = imread('background2.jpg');
 without_background = SubtractBackground(case1, background1);
 %temp_vid = DifferenceBetweenFrames(without_background, 3);
 %[~, ~, ~, x] = size(temp_vid);
 centers = GetObjectsCenters(without_background);
+DrawLine(case1, centers);
